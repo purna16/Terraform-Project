58544cf0a491d519787e4d5055ebb15d055d32ff
Purna Siva Sai <108052304+purna16@users.noreply.github.com>
2023-05-17 11:01:10 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,96 @@
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr_block
+}
+
+resource "aws_subnet" "subnet_1" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.subnet_1_cidr_block
+  availability_zone = var.subnet_1_availability_zone
+}
+
+resource "aws_subnet" "subnet_2" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.subnet_2_cidr_block
+  availability_zone = var.subnet_2_availability_zone
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+}
+
+resource "aws_route_table_association" "subnet_1_association" {
+  subnet_id      = aws_subnet.subnet_1.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_route_table_association" "subnet_2_association" {
+  subnet_id      = aws_subnet.subnet_2.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_security_group" "security_group" {
+  name        = var.security_group_name
+  description = "Allow traffic on port 80"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web_instance_1" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.subnet_1.id
+  vpc_security_group_ids = [aws_security_group.security_group.id]
+}
+
+resource "aws_instance" "web_instance_2" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.subnet_2.id
+  vpc_security_group_ids = [aws_security_group.security_group.id]
+}
+
+resource "aws_lb" "web_elb" {
+  name               = var.elb_name
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
+}
+
+resource "aws_lb_listener" "web_elb_listener" {
+  load_balancer_arn = aws_lb.web_elb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_target_group.arn
+  }
+}
+
+resource "aws_lb_target_group" "web_target_group" {
+  name     = var.target_group_name
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc.id
+}
